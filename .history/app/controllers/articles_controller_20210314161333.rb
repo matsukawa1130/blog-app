9916e98e2d94b11e9df45e3e48ca18033b2297fe
Kazuyuki Matsukawa <kaz19901130@gmail.com>
2021-03-14 17:27:11 +0900

    コミット6

---
+++ .history/app/controllers/articles_controller_20210314161333.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+        @articles = Article.all
+  end
+ def show
+    @article = Article.find(params[:id])
+ end
+
+ def new
+    @article = Article.new
+ end
+
+ def 
+
+ private
+ def article_params
+    params.require(:article).permit(:title, :content)
+ end
+end
---
+++ .history/app/controllers/articles_controller_20210314161337.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+        @articles = Article.all
+  end
+ def show
+    @article = Article.find(params[:id])
+ end
+
+ def new
+    @article = Article.new
+ end
+
+ def create
+    
+
+ private
+ def article_params
+    params.require(:article).permit(:title, :content)
+ end
+end
