1f4df7075a19d4cb0eb5839143888e78a8ea48d1
Kazuyuki Matsukawa <kaz19901130@gmail.com>
2021-03-17 07:41:32 +0900

    部分テンプレートを導入

---
+++ .history/app/models/article_20210314195906.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+
+def display_created_at
+    
+end
---
+++ .history/app/models/article_20210314195909.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+
+def display_created_at
+    I18n.l(@article.created_at, format: :default)
+end
