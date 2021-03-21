cbfd520aa793f332ae4ec633fe818504c540e89a
Kazuyuki Matsukawa <kaz19901130@gmail.com>
2021-03-21 22:00:53 +0900

    no message

---
+++ .history/app/controllers/comments_controller_20210321213124.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+    def new
+      article = Article.find(params[:article_id])
+      @comment = article.comments.build
+    end
+
+
+    def create
+        article.comments.build()
+        comments
+
+    end
+  end
---
+++ .history/app/controllers/comments_controller_20210321213233.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+    def new
+      article = Article.find(params[:article_id])
+      @comment = article.comments.build
+    end
+
+
+    def create
+        article = Artcile.find(params[:article_id])
+        comment = article.comments.build()
+        comments.save
+
+    end
+  end
