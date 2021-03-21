7c46a1580f41e647edc3b1be711b1bd6532a1716
Kazuyuki Matsukawa <kaz19901130@gmail.com>
2021-03-21 12:21:19 +0900

    ログイン必須の機能を実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/ }
+  validates :title, format: { with: /\A(?!@)/ }
 
   validates :content, length: { minimum: 10 }
   validates :content, presence: true
@@ -23,11 +23,10 @@ class Article < ApplicationRecord
     I18n.l(created_at, format: :default)
   end
 
-private
+  private
+
   def validate_title_and_content_length
-      char_count = self.title.length + self.content.length
-      unless char_count > 100
-        errors.add(:content,'100文字以上で！')
+    char_count = title.length + content.length
+    errors.add(:content, '100文字以上で！') unless char_count > 100
   end
-end
 end
