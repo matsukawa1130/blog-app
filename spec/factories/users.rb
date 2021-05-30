d5d346122399f5b2af1e8dca3c7d423f1ba9f165
Kazuyuki Matsukawa <kaz19901130@gmail.com>
2021-05-30 14:38:37 +0900

    system テストを実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :user do
     email {Faker::Internet.email}
-    password { 'password' }     
+    password { 'password' }
+
+    trait :with_profile do
+      after :build do |user|
+        build(:profile, user: user)
+      end
+    end
   end
 end
